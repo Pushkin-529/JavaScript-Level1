ad2cc3e5fd35dd9157a4a342e062dbe500a15b73
Pushkin-529 <t.makhyanov@yandex.ru>
2019-11-28 00:19:41 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,7 +22,26 @@ if ((A>=0)&&(B>=0)) {alert("Задание №3.\r\nРезультат:"+(A-B));
 else if ((A<0)&&(B<0)) {alert("Задание №3.\r\nРезультат:"+(A*B));}
 else alert("Задание №3.\r\nРезультат:"+(A+B));
 console.log("---------------------------------------Задание №4-------------------------------------------");
-console.log("Не понял :-/\r\nОператор в задании №6");
+let num=Number(prompt("Задание №4\r\nВведите число от 0 до 15\r\n"));
+switch (num) {
+    case 0:console.log(num++);
+    case 1:console.log(num++);
+    case 2:console.log(num++);
+    case 3:console.log(num++);
+    case 4:console.log(num++);
+    case 5:console.log(num++);
+    case 6:console.log(num++);
+    case 7:console.log(num++);
+    case 8:console.log(num++);
+    case 9:console.log(num++);
+    case 10:console.log(num++);
+    case 11:console.log(num++);
+    case 12:console.log(num++);
+    case 13:console.log(num++);
+    case 14:console.log(num++);
+    case 15:console.log(num++);break;
+    default:console.log("Error");
+}
 console.log("---------------------------------------Задание №5-------------------------------------------");
 function MathAdd(x,y)
 {
