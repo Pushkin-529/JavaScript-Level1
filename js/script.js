13e0ed79cbaa5df0f2fa05bdc50652534d62f68b
Pushkin-529 <t.makhyanov@yandex.ru>
2019-12-03 04:26:56 +0500

    Lesson-3

--- js/script.js
+++ js/script.js
@@ -1,40 +1,31 @@
-var time=10000; //локальная переменная, но в глобальной области видимости
-var wait_message='Так и будешь смотреть на эту скучную страницу?';
-window.globalOne = 'some value' //Реально глобальная переменная
-const constVar=500; //константа
-let letvar=100;
-var name="Василий";
-var admin;
-var A=20;
-var B=30;
-var celsius=30;
-
-//типы данных:
-// Numbers - числа
-// Strings - строки
-// Boolean - логич
-// Undefined - неопределенный
-// Null - пусто
-// Symbol - символ
-// Object - объект
-// NaN - Not a Number - ошибка сигнализатор
-// Array - Массив => Object
-admin=name;
-
-setTimeout(function(){
-    var localvar="local variable" //локальная переменная
-    alert ('Привет!'+'\r\n'+wait_message);
-},time)
-
-console.log('admin:'+admin);
-console.log(A,B);
-A=A+B;
-B=A-B;
-A=A-B;
-console.log(A,B);
-console.log('1000+\"108\"='+1000+'108');
-
-function getFarengeht(celsius) {
-    return(((9 / 5) * celsius + 32));
+var num=0;
+console.log("---------------------------------------Задание №1-------------------------------------------");
+num=Number(prompt("Задание №1\r\nПоиск простых чисел\r\nВведите число от 0 до 100:\r\n"));
+for(i=0;i<=num;i++)
+{
+    if (checkSimple(i)) console.log(i);
 }
-alert('Конвертер температур (Цельсий/Фарегейт):'+'\r\n'+'Температура C: '+celsius+' градусов'+'\r\n'+'Температура F: '+getFarengeht(celsius)+' градусов');
+
+function checkSimple(num)
+{
+    if (num>2) {
+        for(j=2;j<=num;j++)
+        {
+            if((num%j==0)&&(j<num)) {return(false);}
+        }
+        return(true);
+    }
+    else if(num==2) return(true);
+    else return(false);
+}
+
+console.log("---------------------------------------Задание №4-------------------------------------------");
+for(i=0;i<=9;console.log(i++)){}
+
+console.log("---------------------------------------Задание №4-------------------------------------------");
+let pattern='x';
+for (i=1;i<=20;i++)
+{
+    console.log(pattern);
+    pattern+='x';
+}
